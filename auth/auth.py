abaec213135a64c937817c05edf05cc7c0459a97
inksne <vitalijjapr@gmail.com>
2025-02-26 20:39:39 +0300

    авторизация по jwt

---
+++ auth/auth.py
@@ -0,0 +1,129 @@
+from fastapi import APIRouter, Depends, Response, Form, HTTPException
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.security import HTTPBearer
+from starlette import status
+
+from auth.helpers import create_access_token, create_refresh_token
+from auth.validation import (
+    get_current_access_token_payload,
+    get_current_auth_user_for_refresh,
+    get_current_auth_user,
+    validate_auth_user_db,
+    validate_email
+)
+from auth.utils import hash_password
+
+from database.database import get_async_session
+from database.models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from config import configure_logging
+from exceptions import conflict_name, bad_email_exc, server_exc
+
+from pydantic import BaseModel
+from datetime import timedelta
+import logging
+
+
+http_bearer = HTTPBearer(auto_error=False)
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    refresh_token: str | None = None
+    token_type: str = "Bearer"
+
+
+router = APIRouter(prefix='/jwt', tags=["JWT"], dependencies=[Depends(http_bearer)])
+
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+@router.post('/register')
+async def register(
+    username: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+    session: AsyncSession = Depends(get_async_session)
+):
+    try:
+        hashed_password = hash_password(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_password, email=email)
+
+        validate_email(email)
+
+        session.add(new_user)
+        await session.commit()
+
+        return RedirectResponse('/jwt/login/', status_code=status.HTTP_303_SEE_OTHER)
+    except IntegrityError:
+        await session.rollback()
+        raise conflict_name
+    except HTTPException:
+        await session.rollback()
+        raise bad_email_exc
+    except Exception as e:
+        await session.rollback()
+        logger.error(e)
+        raise server_exc
+
+
+@router.post('/login/', response_model=TokenInfo)
+async def auth_user_issue_jwt(response: Response, user: User = Depends(validate_auth_user_db)):
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+    response.set_cookie(
+        key="access_token",
+          value=access_token,
+            httponly=False,
+              secure=False,
+                samesite="Lax",
+                  max_age=int(timedelta(minutes=5).total_seconds()))
+    
+    response.set_cookie(
+        key="refresh_token",
+          value=refresh_token,
+            httponly=False,
+              secure=False,
+                samesite="Lax",
+                  max_age=int(timedelta(days=30).total_seconds()))
+    
+    return TokenInfo(access_token=access_token, refresh_token=refresh_token)
+
+
+@router.post("/refresh/")
+async def refresh_jwt(current_user: User = Depends(get_current_auth_user_for_refresh)):
+    new_access_token = create_access_token(current_user)
+    response = JSONResponse(content={"access_token": new_access_token})
+    response.set_cookie(
+        key="access_token",
+          value=new_access_token,
+            httponly=False,  
+              secure=False,    
+                samesite="Lax",  
+                  max_age=int(timedelta(minutes=1).total_seconds())) 
+    
+    return response
+
+
+@router.get('/users/me/')
+async def auth_user_check_self_info(
+    payload: dict = Depends(get_current_access_token_payload), 
+    user: User = Depends(get_current_auth_user),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged_in_at": iat,
+    }
+
+
+@router.post('/logout')
+async def logout(response: Response):
+    response.delete_cookie(key="access_token", httponly=False, secure=False, samesite="Lax")
+    response.delete_cookie(key="refresh_token", httponly=False, secure=False, samesite="Lax")
+
+    return
---
+++ auth/validation.py
@@ -0,0 +1,135 @@
+from fastapi import Depends, HTTPException, Request, Form, Cookie
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from starlette import status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+import logging
+
+from auth.helpers import TOKEN_TYPE_FIELD, ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE
+from auth.utils import decode_jwt, decode_jwt_ws, validate_password
+from auth.schemas import UserSchema
+from database.models import User
+from database.database import get_async_session
+from config import configure_logging
+from exceptions import (
+    bad_token_exc,
+    not_found_access_exc,
+    not_found_refresh_exc,
+    not_found_token_user_exc,
+    unauthed_exc,
+    bad_email_exc,
+    ws_unauthorized_none_access,
+    server_exc
+)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/jwt/login")
+
+configure_logging()
+logger = logging.getLogger(__name__)
+
+
+def get_current_access_token_payload(request: Request) -> dict:
+    '''получение access токена'''
+    token = request.cookies.get("access_token")
+    if not token:
+        raise not_found_access_exc
+    
+    try:
+        payload = decode_jwt(token=token)
+    except InvalidTokenError as e:
+        raise bad_token_exc
+    
+    return payload
+
+
+def get_current_refresh_token_payload(request: Request) -> dict:
+    '''получение refresh токена'''
+    token = request.cookies.get("refresh_token")
+    if not token:
+        raise not_found_refresh_exc
+    
+    try:
+        payload = decode_jwt(token=token)
+    except InvalidTokenError as e:
+        raise bad_token_exc
+    
+    return payload
+
+
+def validate_token_type(payload: dict, token_type: str) -> bool:
+    '''проверка на подходящий токен (используется в разных случаях)'''
+    current_token_type = payload.get(TOKEN_TYPE_FIELD)
+    if current_token_type == token_type:
+        return True
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Неверный тип токена {current_token_type!r}, ожидается: {token_type!r}"
+        )
+
+
+def get_current_auth_user_from_access_token_of_type(token_type: str):
+    '''проверка access токена'''
+    async def get_auth_user_from_token(payload: dict = Depends(get_current_access_token_payload), db: AsyncSession = Depends(get_async_session)) -> User:
+        validate_token_type(payload, token_type)
+        return await get_user_by_token_sub(payload, db)
+    return get_auth_user_from_token
+
+
+def get_current_auth_user_from_refresh_token_of_type(token_type: str):
+    '''проверка refresh токена'''
+    async def get_auth_user_from_token(payload: dict = Depends(get_current_refresh_token_payload), db: AsyncSession = Depends(get_async_session)) -> User:
+        validate_token_type(payload, token_type)
+        return await get_user_by_token_sub(payload, db)
+    return get_auth_user_from_token
+
+
+get_current_auth_user = get_current_auth_user_from_access_token_of_type(ACCESS_TOKEN_TYPE)
+
+get_current_auth_user_for_refresh = get_current_auth_user_from_refresh_token_of_type(REFRESH_TOKEN_TYPE)
+
+async def get_user_by_token_sub(payload: dict, db: AsyncSession) -> User:
+    '''получение пользователя по access токену'''
+    username: str | None = payload.get("sub")
+    result = await db.execute(select(User).where(User.username == username))
+    user = result.scalars().first()
+    if user is None:
+        raise not_found_token_user_exc
+
+    return user
+
+
+async def validate_auth_user_db(username: str = Form(...), password: str = Form(...), db: AsyncSession = Depends(get_async_session)):
+    '''валидация введенных пользователем данных (используется для входа)'''
+    user = await db.execute(select(User).where(User.username == username))
+    user = user.scalars().first()  
+
+    if not user:
+        raise unauthed_exc
+    
+    if not validate_password(password=password, hashed_password=user.password.encode('utf-8')):
+        raise unauthed_exc
+    
+    return UserSchema.from_attributes(user)
+
+
+def validate_email(email: str):
+    if email in [None, '', 'null'] or '@' not in email or '.' not in email:
+        raise bad_email_exc
+    
+
+async def ws_verify_user(access_token: str = Cookie(None)):
+    if not access_token:
+        raise ws_unauthorized_none_access
+    
+    try:
+        payload = decode_jwt_ws(access_token)
+        username = payload.get("sub")
+        if not username:
+            raise bad_token_exc
+        return username
+    except Exception as e:
+        logger.error(e)
+        raise server_exc
