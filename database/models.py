8702379fe9b2c1a242e046672628f9c6b1529df8
inksne <vitalijjapr@gmail.com>
2025-03-16 10:10:47 +0300

    убрана TablePlayer

--- database/models.py
+++ database/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Integer, String, Column, ForeignKey
-from sqlalchemy.sql import func
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 
@@ -15,27 +14,16 @@ class User(Base):
     password = Column(String(length=1024), nullable=False)
 
     table = relationship('Table', back_populates='creator')
-    tables = relationship('TablePlayer', back_populates='user')
 
 
 class Table(Base):
     __tablename__ = 'tables'
     id = Column(Integer, primary_key=True)
     name = Column(String(length=32), unique=True, nullable=False)
-    start_money = Column(Integer, default=1000, nullable=False)
-    big_blind = Column(Integer, default=50, nullable=False)
+    start_money = Column(Integer, nullable=False)
+    big_blind = Column(Integer, nullable=False)
     small_blind = Column(Integer, nullable=False)
 
     creator_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     creator = relationship('User', back_populates='table')
-    players = relationship('TablePlayer', back_populates='table')
-
-
-class TablePlayer(Base):
-    __tablename__ = 'table_players'
-    table_id = Column(Integer, ForeignKey('tables.id'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-
-    table = relationship('Table', back_populates='players')
-    user = relationship('User', back_populates='tables')
