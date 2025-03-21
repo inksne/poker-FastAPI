98d34089f97e9fcfb03091eeb132a322694b1905
inksne <vitalijjapr@gmail.com>
2025-03-21 19:41:29 +0300

    удаление: удалены дефолтные значения

--- basemodels.py
+++ basemodels.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class CreateTableRequest(BaseModel):
     name: str
-    start_money: Optional[int] = 1000
-    big_blind: Optional[int] = 50
+    start_money: int
+    big_blind: int
