68ccefd98d91a5d85b673f3362a8b4568a00c7a8
inksne <vitalijjapr@gmail.com>
2025-03-22 20:01:27 +0300

    обновление: изменена отправка игровой стадии

--- game/fold_helper.py
+++ game/fold_helper.py
@@ -1,10 +1,10 @@
 from database.managers import redis_manager
 from config import ws_manager, logger
 
-from .stage_and_turn_helpers import check_all_players_done, proceed_to_next_stage, send_game_stage_global, get_next_turn
+from .stage_and_turn_helpers import check_all_players_done, proceed_to_next_stage, send_game_stage_cards_and_game_started, get_next_turn
 
 
-async def process_fold(players: list[dict], table_id: int) -> None:
+async def process_fold(players: list[dict], table_id: int, community_cards: dict) -> None:
     current_turn = redis_manager.get_current_turn(table_id)
     current_player = players[current_turn]
     username = current_player['username']
@@ -16,7 +16,7 @@ async def process_fold(players: list[dict], table_id: int) -> None:
 
     if all_done:
         await proceed_to_next_stage()
-        await send_game_stage_global()
+        await send_game_stage_cards_and_game_started(community_cards)
 
     next_turn = get_next_turn(players, table_id, current_turn)
     redis_manager.add_current_turn(table_id, next_turn)
