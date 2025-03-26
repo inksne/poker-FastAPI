63004e32c5e949c5e0e829a68b4e5713f55a32e4
inksne <vitalijjapr@gmail.com>
2025-03-26 09:27:56 +0300

    добавление: добавлен classmethod from_attributes (для sqlalchemy)

--- basemodels.py
+++ basemodels.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class CreateTableRequest(BaseModel):
+    model_config = ConfigDict(strict=True, from_attributes=True)
     name: str
     start_money: int
     big_blind: int
+
+    @classmethod
+    def from_attributes(cls, obj):
+        return cls(
+            name=obj.name,
+            start_money=obj.start_money,
+            big_blind=obj.big_blind,
+        )
